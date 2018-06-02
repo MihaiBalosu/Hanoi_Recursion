7ecbbb0a0f42e66e6b10bc52b4a755b662db62e5
MihaiBalosu <32931832+MihaiBalosu@users.noreply.github.com>
2018-06-02 19:41:28 +0300

    First

--- main.c
+++ main.c
@@ -6,9 +6,15 @@
 int n;
 
 int main()
-{
+{   time_t t;
+
+    srand((unsigned) time(&t));
+
     printf( "Enter the number of the disks: \n" );
-    n = rand_gen (argc, **argv);
+
+    n = rand() % 10 ;
+
+    printf("%d", n);
 
     towerOfHanoi( n , 'A' , 'B' , 'C' );
     return 0;
