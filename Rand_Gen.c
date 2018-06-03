8f3a5761c886f169da1beba02fcfad8dc58f9edb
MihaiBalosu <32931832+MihaiBalosu@users.noreply.github.com>
2018-06-03 18:46:05 +0300

    Update4

--- Rand_Gen.c
+++
@@ -1,7 +0,0 @@
-#include <Rand_Gen.h>
-
-    int rand_gen (int argc, char **argv);
-    {
-
-        return rand();
-    }
--- Rand_Gen.h
+++
@@ -1,6 +0,0 @@
-#ifndef RAND_GEN_H_INCLUDED
-#define RAND_GEN_H_INCLUDED
-
-int rand_gen (int argc, char **argv);
-
-#endif // RAND_GEN_H_INCLUDED
